289b0b0cde345ef32ba53bbc5a3d32210493849b
복정규 <_jk_@daum.net>
2021-10-29 10:02:57 +0900

    실습1-webpack/1-entry 완료

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode : 'development',
+  entry : {
+    main : "./src/app.js"
+  },
+  output : {
+    path: path.resolve('./dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use:[
+          {
+            loader : path.resolve('./my-webpack-loader.js')
+          }
+        ]
+      },
+      {
+        test : /\.css$/,
+        use : [
+          'css-loader'
+        ]
+      }
+    ]
+  }
+}
