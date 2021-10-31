958a955de099feb0e121f8a0fe0e27cb1461e42d
복정규 <_jk_@daum.net>
2021-10-31 15:56:49 +0900

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,38 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module : {
+    rules:[
+      {
+        test : /\.css$/,
+        use : ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use : [
+          {
+            loader : 'url-loader',
+            options: {
+              publicPath: "./dist/",
+              name: 'images/[name].[ext]?[hash]',
+              limit: 10000 // 10kb 미만 파일만 data url로 처리
+            },
+          }
+        ]
+      }
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use : [
+      //     {
+      //       loader : 'file-loader',
+      //       options: {
+      //         publicPath: "./dist/",
+      //         name: 'images/[name].[ext]?[hash]',
+      //       },
+      //     }
+      //   ]
+      // },
+    ]
   }
 }
